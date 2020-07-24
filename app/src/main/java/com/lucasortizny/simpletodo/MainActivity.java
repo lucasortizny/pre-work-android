4a15dc5a5ff34dfa389b1327a9edde5c1265850f
lucasortizny <38075213+lucasortizny@users.noreply.github.com>
2020-07-24 01:22:28 -0400

    Instantialized Git Repository from within Android Studio

---
+++ app/src/main/java/com/lucasortizny/simpletodo/MainActivity.java
@@ -0,0 +1,96 @@
+package com.lucasortizny.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    List<String> items;
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+    itemsAdapter ia;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAdd = findViewById(R.id.btnAdd); //Convert Add Button to Java Object
+        etItem = findViewById(R.id.etItem); //Convert TextView to Java Object
+        rvItems = findViewById(R.id.rvItems); //Convert RecyclerView as Java Obj
+        loadItems();
+
+
+
+        etItem.setText("I am doing this from java");
+        itemsAdapter.OnLongClickListener onLongClickListener = new itemsAdapter.OnLongClickListener() {
+            @Override
+            public void onItemLongClicked(int position) {
+                Toast.makeText(getApplicationContext(), String.format("Item '%s' was removed", items.get(position)), Toast.LENGTH_SHORT).show();
+                items.remove(position);
+
+                //Must notify Adapter
+                ia.notifyItemRemoved(position);
+                saveItems();
+
+            }
+        };
+        ia = new itemsAdapter(items, onLongClickListener);
+        rvItems.setAdapter(ia);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = etItem.getText().toString();
+                items.add(text);
+                ia.notifyItemInserted(items.size() - 1); //This sends notification to our adapter
+
+                etItem.setText(""); //clear item
+                //Toast to reply back the part of the list that was added.
+                Toast.makeText(getApplicationContext(), String.format("Item '%s' was added", items.get(items.size()-1)), Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
+
+
+
+    }
+
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+
+    }
+    private void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Unable to read file data correctly");
+            items = new ArrayList<>();
+        }
+        
+
+    }
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "error saving items to local storage");
+        }
+    }
+
+}
---
+++ app/src/main/java/com/lucasortizny/simpletodo/itemsAdapter.java
@@ -0,0 +1,68 @@
+package com.lucasortizny.simpletodo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class itemsAdapter extends RecyclerView.Adapter<itemsAdapter.ViewHolder> {
+    public interface OnLongClickListener {
+        void onItemLongClicked(int position);
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+    public itemsAdapter(List<String> items, OnLongClickListener longClickListener) {
+        this.items = items;
+        this.longClickListener = longClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View todoview = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(todoview);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Grab item at the position
+        String item = items.get(position);
+        //Binding
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        //This is doable because the List structure has this method that exists.
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textviewitem;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textviewitem = itemView.findViewById(android.R.id.text1);
+        }
+
+        public void bind(String item) {
+            textviewitem.setText(item);
+            textviewitem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
+
+
+        }
+    }
+
+
+}
